54bf60717abaffb2284264e1faef8bb232bf40e1
sksamuel <sam@sksamuel.com>
2018-03-15 17:10:17 +0000

    Added insert check to insert test

--- src/test/kotlin/com/landoop/jdbc4/InsertTest.kt
+++ src/test/kotlin/com/landoop/jdbc4/InsertTest.kt
@@ -18,12 +18,32 @@ class InsertTest : WordSpec(), CCData {
         val sql = "INSERT INTO cc_data (customerFirstName, number, currency, customerLastName, country, blocked) values ('${card.firstname}', ${card.number}, '${card.currency}' ,'${card.surname}', '${card.country}', ${card.blocked})"
         val stmt = conn.createStatement()
         stmt.execute(sql) shouldBe true
+
+        // now we must check that our values have been inserted
+        val rs = conn.createStatement().executeQuery("SELECT * FROM cc_data WHERE _ktype='STRING' AND _vtype='AVRO' AND customerLastName='${card.surname}' and customerFirstName='${card.firstname}' AND number='${card.number}'")
+        rs.next()
+        rs.getString("customerFirstName") shouldBe card.firstname
+        rs.getString("customerLastName") shouldBe card.surname
+        rs.getString("number") shouldBe card.number
+        rs.getString("currency") shouldBe card.currency
+        rs.getString("country") shouldBe card.country
+        rs.getBoolean("blocked") shouldBe card.blocked
       }
       "support insertion mixed case" {
         val card = generateCC()
         val sql = "inSerT INTO cc_data (customerFirstName, number, currency, customerLastName, country, blocked) values ('${card.firstname}', ${card.number}, '${card.currency}' ,'${card.surname}', '${card.country}', ${card.blocked})"
         val stmt = conn.createStatement()
         stmt.execute(sql) shouldBe true
+
+        // now we must check that our values have been inserted
+        val rs = conn.createStatement().executeQuery("SELECT * FROM cc_data WHERE _ktype='STRING' AND _vtype='AVRO' AND customerLastName='${card.surname}' and customerFirstName='${card.firstname}' AND number='${card.number}'")
+        rs.next()
+        rs.getString("customerFirstName") shouldBe card.firstname
+        rs.getString("customerLastName") shouldBe card.surname
+        rs.getString("number") shouldBe card.number
+        rs.getString("currency") shouldBe card.currency
+        rs.getString("country") shouldBe card.country
+        rs.getBoolean("blocked") shouldBe card.blocked
       }
     }
   }
