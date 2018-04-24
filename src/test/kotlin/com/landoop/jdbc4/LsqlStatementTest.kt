028eea1b0bf2e8c8ffbbda0a43d37c94c6d4fa4f
sksamuel <sam@sksamuel.com>
2018-04-25 00:31:22 +0100

    Updated LsqlStatement test

--- src/test/kotlin/com/landoop/jdbc4/LsqlStatementTest.kt
+++ src/test/kotlin/com/landoop/jdbc4/LsqlStatementTest.kt
@@ -3,6 +3,7 @@ package com.landoop.jdbc4
 import io.kotlintest.shouldThrow
 import io.kotlintest.specs.WordSpec
 import java.sql.DriverManager
+import java.sql.SQLFeatureNotSupportedException
 import java.sql.Statement
 
 class LsqlStatementTest : WordSpec() {
@@ -12,13 +13,13 @@ class LsqlStatementTest : WordSpec() {
     "LsqlStatement" should {
       "throw exception for execute with auto generated columns" {
         val conn = DriverManager.getConnection("jdbc:lsql:kafka:http://localhost:3030", "admin", "admin")
-        shouldThrow<Exception> {
+        shouldThrow<SQLFeatureNotSupportedException> {
           conn.createStatement().execute("select * from table", Statement.RETURN_GENERATED_KEYS)
         }
-        shouldThrow<Exception> {
+        shouldThrow<SQLFeatureNotSupportedException> {
           conn.createStatement().execute("select * from table", intArrayOf(1))
         }
-        shouldThrow<Exception> {
+        shouldThrow<SQLFeatureNotSupportedException> {
           conn.createStatement().execute("select * from table", arrayOf("a"))
         }
       }
