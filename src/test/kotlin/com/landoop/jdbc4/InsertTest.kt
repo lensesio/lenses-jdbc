9a0cbebae79cc5ec3e4002d8ea101ba6f39048aa
sksamuel <sam@sksamuel.com>
2018-04-25 01:25:26 +0100

    Added exception catcher

--- src/test/kotlin/com/landoop/jdbc4/InsertTest.kt
+++ src/test/kotlin/com/landoop/jdbc4/InsertTest.kt
@@ -34,20 +34,25 @@ class InsertTest : WordSpec(), CCData {
         }
       }
       "support insertion mixed case" {
-        val card = generateCC()
-        val sql = "inSerT INTO cc_data (customerFirstName, number, currency, customerLastName, country, blocked) values ('${card.firstname}', ${card.number}, '${card.currency}' ,'${card.surname}', '${card.country}', ${card.blocked})"
-        val stmt = conn.createStatement()
-        stmt.execute(sql) shouldBe true
+        try {
+          val card = generateCC()
+          val sql = "inSerT INTO cc_data (customerFirstName, number, currency, customerLastName, country, blocked) values ('${card.firstname}', ${card.number}, '${card.currency}' ,'${card.surname}', '${card.country}', ${card.blocked})"
+          val stmt = conn.createStatement()
+          stmt.execute(sql) shouldBe true
 
-        // now we must check that our values have been inserted
-        val rs = conn.createStatement().executeQuery("SELECT * FROM cc_data WHERE _ktype='STRING' AND _vtype='AVRO' AND customerLastName='${card.surname}' and customerFirstName='${card.firstname}' AND number='${card.number}'")
-        rs.next()
-        rs.getString("customerFirstName") shouldBe card.firstname
-        rs.getString("customerLastName") shouldBe card.surname
-        rs.getString("number") shouldBe card.number
-        rs.getString("currency") shouldBe card.currency
-        rs.getString("country") shouldBe card.country
-        rs.getBoolean("blocked") shouldBe card.blocked
+          // now we must check that our values have been inserted
+          val rs = conn.createStatement().executeQuery("SELECT * FROM cc_data WHERE _ktype='STRING' AND _vtype='AVRO' AND customerLastName='${card.surname}' and customerFirstName='${card.firstname}' AND number='${card.number}'")
+          rs.next()
+          rs.getString("customerFirstName") shouldBe card.firstname
+          rs.getString("customerLastName") shouldBe card.surname
+          rs.getString("number") shouldBe card.number
+          rs.getString("currency") shouldBe card.currency
+          rs.getString("country") shouldBe card.country
+          rs.getBoolean("blocked") shouldBe card.blocked
+        } catch (t: Throwable) {
+          t.printStackTrace()
+          throw t
+        }
       }
     }
   }
